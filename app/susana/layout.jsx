1e51e64e018dea5b3f54f52b245e978a796f7655
angelruuiz <angellruuizgarcia@gmail.com>
2026-08-07 12:05:55 +0200

    feat: add /susana apology form page

---
+++ app/susana/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Formulario Oficial de Disculpas',
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function SusanaLayout({ children }) {
+  return children;
+}
